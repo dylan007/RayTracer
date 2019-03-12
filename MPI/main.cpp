84f0feb3d5537ad554f992e7997697d31312c591
dylan007 <dylandey1996@gmail.com>
2019-03-12 22:43:54 +0530

    Added batch processing using MPI

--- MPI/main.cpp
+++ MPI/main.cpp
@@ -1,13 +1,15 @@
 #include<bits/stdc++.h>
 #include<math.h>
 #include<mpi.h>
+#include "../library/tracer.hpp"
 
 using namespace std;
 
 //Define hyperparameters
-#define NX 20
-#define NY 10
+#define NX 1000
+#define NY 500
 #define NS 10
+#define CONST 10000
 
 int main(int argc,char *argv[]){
     //Initializing the MPI Context
@@ -20,21 +22,21 @@ int main(int argc,char *argv[]){
 
 
     //Initializing common variables for each process.
-    int image[NX][NY][3];
-    int area = NX * NY;
-    int work_area = area / size;
-    int wx = (int)sqrt(size);       //Assuming that the number of procs is a perfect square.
-    int wy = (int)sqrt(size);       //Number of workers in each row/column.
-    int sx = NX/wx;                 //Size of each row.
-    int sy = NY/wy;                 //Size of each column.
-    int startx = (rank / wy) * sx;
-    int starty = (rank % wx) * sy;
-    int lenx = sx;
-    int leny = sy;
-    int pimage[lenx][leny][3];
-    out = to_string(rank) + " " + to_string(startx) + " " + to_string(starty) + " " + to_string(lenx) + " " + to_string(leny) + "\n";
-    cout << out;
-
+    // Definitions for subblocks
+    // int image[NX][NY][3];
+    // int area = NX * NY;
+    // int work_area = area / size;
+    // int wx = (int)sqrt(size);       //Assuming that the number of procs is a perfect square.
+    // int wy = (int)sqrt(size);       //Number of workers in each row/column.
+    // int sx = NX/wx;                 //Size of each row.
+    // int sy = NY/wy;                 //Size of each column.
+    // int startx = (rank / wy) * sx;
+    // int starty = (rank % wx) * sy;
+    // int lenx = sx;
+    // int leny = sy;
+    // int pimage[lenx][leny][3];
+    // out = to_string(rank) + " " + to_string(startx) + " " + to_string(starty) + " " + to_string(lenx) + " " + to_string(leny) + "\n";
+    // cout << out;
 
     //Define Custom Datatypes
     // MPI_Datatype block,rblock;
@@ -43,20 +45,10 @@ int main(int argc,char *argv[]){
     // int starts[3] = {0,0,0};
     // MPI_Type_create_subarray(3,sizes,subsizes,starts,MPI_ORDER_C,MPI_INT,&block);
     // MPI_Type_create_resized(block,0,sy*3,&rblock);
-    // MPI_Type_commit(&rblock);    
+    // MPI_Type_commit(&rblock);  
 
 
-    // Calculate the pixel values.
-    for(int i=startx;i<=startx+lenx-1;i++){
-        for(int j=starty;j<=starty+leny-1;j++){
-            pimage[i][j][0] = rank;
-            pimage[i][j][1] = rank;
-            pimage[i][j][2] = rank;
-            out = to_string(image[i][j][0]) + " " + to_string(image[i][j][1]) + " " + to_string(image[i][j][2]) + "\n";
-            cout << out;
-        }
-    }
-    // MPI_Barrier(MPI_COMM_WORLD);
+    //Gather Using a custom datatype
     // int sendcounts[wx*wy];
     // for(int i=0;i<wx*wy;i++) sendcounts[i] = 1;
     // int displs[wx*wy];
@@ -71,21 +63,63 @@ int main(int argc,char *argv[]){
     // //Gather from all the other processes.
     // MPI_Gatherv(&pimage,sx*sy*3,MPI_INT,&image,sendcounts,displs,rblock,0,MPI_COMM_WORLD);
 
+    // Definitions for rowblocks
+    int image[NX][NY][3];
+    int area = NX*NY;
+    int work_area = area/size;
+    int wx = size;
+    int wy = 1;
+    int sx = NX/wx;
+    int sy = NY/wy;
+    int startx = rank * sx;
+    int starty = 0;
+    int lenx = sx;
+    int leny = sy;
+    int pimage[lenx][leny][3];
+    vector<int> rgb(3);
+    vector<vector<int>> row(leny,rgb);
+    vector<vector<vector<int>>> res(lenx,row);
+    // out = to_string(rank) + " " + to_string(startx) + " " + to_string(starty) + " " + to_string(lenx) + " " + to_string(leny) + "\n";
+    // cout << out;  
 
-    //Generate the image in process with rank 0.
-    // if(rank==root){
-    //     out = "P3\n" + to_string(NX) + " " + to_string(NY) + "\n255\n";
-    //     cout << out; 
-    //     for(int i=0;i<NX;i++){
-    //         for(int j=0;j<NY;j++){
-    //             out = to_string(i) + " " + to_string(j) + "\n";
-    //             cout << out;
-                // out = to_string(image[i][j][0]) + " " + to_string(image[i][j][1]) + " " + to_string(image[i][j][2]) + "\n";
-    //             // cout << image[i][j][0] << " " << image[i][j][1] << " " << image[i][j][2] << endl;
-    //             cout << out;
-    //         }
+
+    // Calculate the pixel values.
+    // for(int i=startx;i<=startx+lenx-1;i++){
+    //     for(int j=starty;j<=starty+leny-1;j++){
+    //         pimage[i][j][0] = ((double)rank/(double)size)*255.99;
+    //         pimage[i][j][1] = ((double)rank/(double)size)*255.99;
+    //         pimage[i][j][2] = ((double)rank/(double)size)*255.99;
+    //         // out = to_string(pimage[i][j][0]) + " " + to_string(pimage[i][j][1]) + " " + to_string(pimage[i][j][2]) + "\n";
+    //         // cout << out;
     //     }
     // }
+    run(NX,NY,NS,startx,starty,lenx,leny,res);
+    for(int i=0;i<lenx;i++){
+        for(int j=0;j<leny;j++){
+            for(int k=0;k<3;k++)
+                pimage[i][j][k] = res[i][j][k];
+        }
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    //Gather row wise
+    MPI_Gather(pimage,lenx*leny*3,MPI_INT,image,lenx*leny*3,MPI_INT,0,MPI_COMM_WORLD);
+
+    // Generate the image in process with rank 0.
+    if(rank==root){
+        // MPI_Gather(pimage,lenx*leny*3,MPI_INT,image,lenx*leny*3,MPI_INT,0,MPI_COMM_WORLD);
+        out = "P3\n" + to_string(NX) + " " + to_string(NY) + "\n255\n";
+        cout << out; 
+        for(int j=NY-1;j>=0;j--){
+            for(int i=0;i<NX;i++){
+                // out = to_string(i) + " " + to_string(j) + "\n";
+                // cout << out;
+                out = to_string(image[i][j][0]) + " " + to_string(image[i][j][1]) + " " + to_string(image[i][j][2]) + "\n";
+                // cout << image[i][j][0] << " " << image[i][j][1] << " " << image[i][j][2] << endl;
+                cout << out;
+            }
+        }
+    }
     MPI_Finalize();
     return 0;
 }
---
+++ library/tracer.hpp
@@ -0,0 +1,87 @@
+#include<iostream>
+#include "sphere.hpp"
+#include "hitable_list.hpp"
+#include "camera.hpp"
+#include "material.hpp"
+#include<math.h>
+using namespace std;
+
+#define CONST 10000
+
+// float hit_sphere(vec center,float radius,ray r){
+//     vec oc = r.origin() - center;
+//     float a = dot(r.B,r.B);
+//     float b = 2.0*dot(r.B,oc);
+//     float c = dot(oc,oc) - radius * radius;
+//     float disc = b*b - 4.0*a*c;  
+//     if(disc>=0)
+//         return (-b - sqrt(disc))/(2.0*a);
+//     else
+//         return -1.0;
+// }
+
+// vec color(ray r){
+//     float ret = hit_sphere(vec(0,0,-1),0.5,r);
+//     // cout << ret << endl;
+//     if(ret>0.0){
+//         vec N = r.get_point(ret) - vec(0,0,-1);
+//         if(N.length()!=0)
+//             N.normalize();
+//         return 0.5 * vec(N.x + 1,N.y + 1,N.z + 1);
+//     }
+//     vec unit_dir = r.direction();
+//     unit_dir.normalize();
+//     float t = 0.5 * (unit_dir.y + 1.0);
+//     return (1-t) * vec(1.0,1.0,1.0) + t * vec(1,0,0);
+// }
+
+
+vec color(ray &r, hitable *world,int depth){
+    hit_record rec;
+    if(world->hit(r,0.001,MAXFLOAT,rec)){
+        ray scattered;
+        vec attenuation;
+        if(depth<50 && rec.mat_ptr->scatter(r,rec,attenuation,scattered))
+            return attenuation * color(scattered,world,depth+1);
+        else
+            return vec(0,0,0);
+        // vec target = rec.p + rec.normal + random_in_unit_sphere();
+        // ray tmp = ray(rec.p,target-rec.p);
+        // return 0.5 * color(tmp, world);
+        // return 0.5 * vec(rec.normal.x + 1,rec.normal.y + 1 , rec.normal.z + 1);
+    }
+    else{
+        vec unit_dir = r.direction();
+        float t = 0.5 * (unit_dir.y + 1);
+        return (1.0 - t) * vec(1,1,1) + t * vec(0.5,0.7,1.0);
+    }
+}
+
+void run(int nx,int ny,int ns,int startx,int starty,int lenx,int leny,vector<vector<vector<int>>> &res){
+    // cout << "P3\n" << nx << " " << ny << "\n255\n";
+    hitable *list[4];
+    list[0] = new sphere(vec(0,0,-1),0.5,new lambertian(vec(0.8,0.3,0.3)));
+    list[1] = new sphere(vec(0,-100.5,-1),100.0,new lambertian(vec(0.8,0.8,0.0)));
+    list[2] = new sphere(vec(1,0,-1),0.5,new metal(vec(0.8,0.6,0.2)));
+    list[3] = new sphere(vec(-1,0,-1),0.5,new metal(vec(0.8,0.8,0.8)));
+    hitable *world = new hitable_list(list,4);
+    camera cam;
+    for(int j=starty+leny-1;j>=starty;j--){
+        for(int i=startx;i<startx+lenx;i++){
+            vec col(0,0,0);
+            for(int s=0;s < ns;s++){
+                float u = float(i + rn())/float(nx);
+                float v = float(j + rn())/float(ny);
+                ray r = cam.get_ray(u,v);
+                vec p = r.get_point(2.0);
+                col += color(r,world,0);
+            }
+            col /= ns;
+            col = vec(sqrt(col.x),sqrt(col.y),sqrt(col.y));
+            res[i-startx][j-starty][0] = int(255.99 * col.x);
+            res[i-startx][j-starty][1] = int(255.99 * col.y);
+            res[i-startx][j-starty][2] = int(255.99 * col.z);
+            // cout << ir << " " << ig << " " << ib << endl;
+        }
+    }
+}
